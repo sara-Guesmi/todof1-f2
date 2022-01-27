bd5ccb606340e9fc33bd53a85257c0feba475b0c
sarra-94 <gusmi.sarra@gmail.com>
2022-01-27 14:12:18 +0100

    f1 changes

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -8,22 +8,28 @@ const Task = ({ task }) => {
   const [newText, setnewText] = useState(task.text);
   const [edit, setEdit] = useState(false);
   const dispatch = useDispatch();
+  // function to dispatch the check action
   const handleCheck = () => {
     dispatch(checkTask(task.id));
   };
+  // function to dispatch the delete action
   const handleDelete = () => {
     dispatch(deleteTask(task.id));
   };
+  // when i click to the edit button (if true show the input else show the span)
   const handleEdit = () => {
     setEdit(!edit);
   };
+  // function to handle the data in the input
   const handleText = (e) => {
     setnewText(e.target.value);
   };
+  //function to dispatch the action edit to the reducer
   const handleSave = () => {
     dispatch(editTask(task.id, newText));
     setEdit(false);
   };
+  // function to close the edit bloc
   const handleClose = () => {
     setEdit(false);
     setnewText(task.text);
@@ -45,6 +51,7 @@ const Task = ({ task }) => {
           <button onClick={handleSave}>save</button>
         </div>
       )} */}
+      {/* method 3 using router */}
       <Link to={`/edit/${task.id}`}>
         {" "}
         <button onClick={handleEdit}>edit</button>
